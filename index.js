a5eaa65c5e7d575b8f89bf4a2570219e6fefbc96
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-12-02 12:42:31 +0330

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("body", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
