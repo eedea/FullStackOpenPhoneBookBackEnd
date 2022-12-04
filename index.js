a2cabafe2b2e394e0a6c8d5d87046319fb5ca5e6
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-12-04 23:33:32 +0330

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+const cors = require("cors");
+app.use(cors());
 
+const morgan = require("morgan");
 app.use(express.json());
 morgan.token("body", function (req, res) {
   if (req.method === "POST") return JSON.stringify(req.body);
